b1cb2c8ebee28ebeb6be52249ef733969dddf35d
DamBeDev <143622448+DamBeDev@users.noreply.github.com>
2025-05-28 19:44:50 -0300

    feat: Unit test with "getByRole" in GifItem component

--- tests/components/GifItem.test.jsx
+++ tests/components/GifItem.test.jsx
@@ -1,4 +1,4 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { GifItem } from "../../src/components/GifItem";
 
 describe('Pruebas en <GifItem />', () => {
@@ -11,5 +11,20 @@ describe('Pruebas en <GifItem />', () => {
     const { container } = render( <GifItem title={ title } url={ url }/> );
     expect( container ).toMatchSnapshot();
 
+  });
+
+  test('Debe de mostrar la imagen con el URL y el ALT indicado', () => {
+
+    render( <GifItem title={ title } url={ url }/> );
+    expect( screen.getByRole('img').src ).toBe( url );
+    expect( screen.getByRole('img').alt ).toBe( title );
+
+  });
+  
+  test('Debe de mostrar el título en el componente', () => {
+
+    render( <GifItem title={ title } url={ url }/> );
+    expect(screen.getByText( title )).toBeTruthy();
+    
   });
 });
